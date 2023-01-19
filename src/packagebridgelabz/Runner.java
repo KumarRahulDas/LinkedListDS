d95339c29b5e4d4c1c33013f4082c550dbf415f7
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-01-19 02:28:59 -0800

    Adding Simple Linked List

---
+++ src/packagebridgelabz/Runner.java
@@ -0,0 +1,11 @@
+package bridgelabz;
+
+public class Runner {
+        public static void main(String[] args) {
+            LinkedList<Integer> numberList = new LinkedList<>();
+            numberList.add(56);
+            numberList.add(30);
+            numberList.add(70);
+            numberList.show();
+        }
+}
